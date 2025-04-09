d1dc3d68eb37fbeba5670c9311a389e728262a8e
William Roberts <werobert@yahoo.com>
2025-04-09 11:16:43 -0400

    product-category table

--- backend/instruments.sql
+++ backend/instruments.sql
@@ -6,43 +6,52 @@ DROP SCHEMA IF EXISTS `music-shop-db`;
 CREATE SCHEMA `music-shop-db`;
 USE `music-shop-db` ;
 
+-- -----------------------------------------------------
+-- Table `music-shop-db`.`product_category`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `music-shop-db`.`product_category` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `category_name` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE=InnoDB
+AUTO_INCREMENT = 1;
 
 -- -----------------------------------------------------
--- Table `full-stack-ecommerce`.`instruments`
+-- Table `music-shop-db`.`products`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `music-shop-db`.`instruments` (
+CREATE TABLE IF NOT EXISTS `music-shop-db`.`products` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) DEFAULT NULL,
   `description` VARCHAR(255) DEFAULT NULL,
   `price` DECIMAL(13,2) DEFAULT NULL,
   `image_url` VARCHAR(255) DEFAULT NULL,
   `stock` INT(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `category_id` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_category` (`category_id`),
+  CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `product_category` (`id`)
 ) 
 ENGINE=InnoDB
 AUTO_INCREMENT = 1;
 
-
 -- -----------------------------------------------------
--- Add sample data
+--sample data
 -- -----------------------------------------------------
 
-INSERT INTO instruments (name, description, image_url, stock, price)
+INSERT INTO product_category(category_name) VALUES ('INSTRUMENTS');
+
+INSERT INTO products (name, description, image_url, stock, price, category_id)
 VALUES ('Gibson Guitar', 'Les Paul 1980 sunburst',
-'assets/images/products/placeholder.png', 5, 1200.99);
+'assets/images/products/placeholder.png', 5, 1200.99, 1);
 
-INSERT INTO instruments (name, description, image_url, stock,
-price)
-VALUES ('Yamaha Piano', 'Electric', 'assets/images/products/placeholder.png', 15, 600.99);
+INSERT INTO products (name, description, image_url, stock, price, category_id)
+VALUES ('Yamaha Piano', 'Electric', 'assets/images/products/placeholder.png', 15, 600.99, 1);
 
-INSERT INTO instruments (name, description, image_url, stock,
-price)
-VALUES ('Kawai Piano', 'Baby-grand', 'assets/images/products/placeholder.png', 3, 4000.99);
+INSERT INTO products (name, description, image_url, stock, price, category_id)
+VALUES ('Kawai Piano', 'Baby-grand', 'assets/images/products/placeholder.png', 3, 4000.99, 1);
 
-INSERT INTO instruments (name, description, image_url, stock,
-price)
-VALUES ('Fender', 'Strat', 'assets/images/products/placeholder.png', 12, 1100.99);
+INSERT INTO products (name, description, image_url, stock, price, category_id)
+VALUES ('Fender', 'Strat', 'assets/images/products/placeholder.png', 12, 1100.99, 1);
 
-INSERT INTO instruments (name, description, image_url, stock,
-price)
-VALUES ('Pearl Drums', 'Full-set with cymbals', 'assets/images/products/placeholder.png', 7, 500.99);
+INSERT INTO products (name, description, image_url, stock, price, category_id)
+VALUES ('Pearl Drums', 'Full-set with cymbals', 'assets/images/products/placeholder.png', 7, 500.99, 1);
--- backend/src/main/java/com/example/demo/entity/Instrument.java
+++ backend/src/main/java/com/example/demo/entity/Instrument.java
@@ -1,28 +1,28 @@
-package main.java.com.example.demo.entity;
+// package main.java.com.example.demo.entity;
 
-import java.math.BigDecimal;
-import javax.annotation.processing.Generated;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+// import java.math.BigDecimal;
+// import javax.annotation.processing.Generated;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "instrument")
-@Data
-public class Instrument {
+// @Entity
+// @Table(name = "instrument")
+// @Data
+// public class Instrument {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.Identity)
-    @Column(name = "id")
-    private Long id;
-    @Column(name = "name")
-    private String name;
-    @Column(name = "description")
-    private String description;
-    @Column(name = "price")
-    private BigDecimal price;
-    @Column(name = "image_url")
-    private String imageUrl;
-    @Column(name = "stock")
-    private int stock;
+// @Id
+// @GeneratedValue(strategy = GenerationType.Identity)
+// @Column(name = "id")
+// private Long id;
+// @Column(name = "name")
+// private String name;
+// @Column(name = "description")
+// private String description;
+// @Column(name = "price")
+// private BigDecimal price;
+// @Column(name = "image_url")
+// private String imageUrl;
+// @Column(name = "stock")
+// private int stock;
 
-}
+// }
