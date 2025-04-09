a3fb6e12d9c56e2129fd237e85ea3664fb6ac5ea
William Roberts <werobert@yahoo.com>
2025-04-09 15:08:57 -0400

    productcategoryrepo

---
+++ backend/src/main/java/com/example/demo/dao/ProductCategoryRepository.java
@@ -0,0 +1,10 @@
+package main.java.com.example.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import main.java.com.example.demo.entity.ProductCategory;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+}
--- backend/src/main/java/com/example/demo/dao/ProductRepository.java
+++ backend/src/main/java/com/example/demo/dao/ProductRepository.java
@@ -1,5 +1,8 @@
 package main.java.com.example.demo.dao;
 
+import main.java.com.example.demo.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
