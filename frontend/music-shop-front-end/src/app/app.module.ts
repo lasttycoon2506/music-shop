29f0f15fab52e156350f4a1feab036e1ae1f13ea
William Roberts <werobert@yahoo.com>
2025-04-09 17:25:09 -0400

    product-service

--- frontend/music-shop-front-end/src/app/app.module.ts
+++ frontend/music-shop-front-end/src/app/app.module.ts
@@ -3,15 +3,16 @@ import {
   BrowserModule,
   provideClientHydration,
 } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { provideHttpClient } from '@angular/common/http';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [provideClientHydration()],
+  providers: [provideClientHydration(), provideHttpClient(), ProductService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
