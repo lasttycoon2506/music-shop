f659ef07060890073ccfe5b8b4ac2c921eac78a1
William Roberts <werobert@yahoo.com>
2025-04-10 17:24:56 -0400

    side-bar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NavBarVerticalComponent } from './components/nav-bar-vertical/nav-bar-vertical.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, NavBarVerticalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ frontend/src/app/components/nav-bar-vertical/nav-bar-vertical.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nav-bar-vertical',
+  imports: [],
+  templateUrl: './nav-bar-vertical.component.html',
+  styleUrl: './nav-bar-vertical.component.css',
+})
+export class NavBarVerticalComponent {}
