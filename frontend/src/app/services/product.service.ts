d42e96cee5e59437866dd75833fda1734e8d30e7
William Roberts <werobert@yahoo.com>
2025-04-10 13:44:36 -0400

    fe-env api-url

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -2,20 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { Product } from '../models/product';
 import { ApiResponse } from '../models/apiResponse';
+import { environment } from '../../environment/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   private httpClient = inject(HttpClient);
+  private baseUrl = environment.apiUrl;
   products = signal<Product[] | null>(null);
-  private baseUrl = 'http://localhost:8080/api/products';
 
   getProducts() {
-    this.httpClient.get<ApiResponse>(this.baseUrl).subscribe({
+    this.httpClient.get<ApiResponse>(this.baseUrl + 'products').subscribe({
       next: (res) => {
         this.products.set(res._embedded.products);
-        console.log(res._embedded);
       },
     });
   }
