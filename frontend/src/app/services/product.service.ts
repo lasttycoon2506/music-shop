5bc77a14ba9d56b6ff9d201df411db11e7ca5397
William Roberts <werobert@yahoo.com>
2025-04-10 11:34:16 -0400

    getProducts method

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +8,13 @@ import { inject, Injectable, signal } from '@angular/core';
 export class ProductService {
   private apiUrl: string = 'http://localhost:8080/api/products';
   private httpClient = inject(HttpClient);
-  products = signal<Product[]>;
+  products = signal<Product[] | null>(null);
+
+  getProducts() {
+    this.httpClient.get<Product[]>(this.apiUrl).subscribe({
+      next: (res) => {
+        this.products.set(res);
+      },
+    });
+  }
 }
