ec3d4ee5d6b3277a133142851e795d5194631e34
William Roberts <werobert@yahoo.com>
2025-04-11 16:22:45 -0400

    static pic resize

--- frontend/src/app/components/product-card/product-card.component.ts
+++ frontend/src/app/components/product-card/product-card.component.ts
@@ -1,13 +1,15 @@
 import { Component, input } from '@angular/core';
 import { Product } from '../../models/product';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'product-card',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
 export class ProductCardComponent {
   product = input.required<Product>();
+  productIndex = input.required<number>();
 }
