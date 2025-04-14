868b89f13ff8e252c2947925501440110ecf8d95
William Roberts <werobert@yahoo.com>
2025-04-14 14:32:20 -0400

    sidebar init product-categories

--- frontend/src/app/components/side-bar/side-bar.component.ts
+++ frontend/src/app/components/side-bar/side-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductCategory } from '../../models/productCategory';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'side-bar',
@@ -6,4 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css',
 })
-export class SideBarComponent {}
+export class SideBarComponent implements OnInit {
+  private productService = inject(ProductService);
+  productCategories: ProductCategory[] = [];
+
+  ngOnInit(): void {
+    this.loadProductCategories();
+  }
+
+  loadProductCategories(): void {
+    this.productService.getProductCategories().subscribe({
+      next: (data) => {
+        this.productCategories = data;
+      },
+    });
+  }
+}
