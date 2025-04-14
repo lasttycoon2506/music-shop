f09dd5bb2ce5657bd8e5a97791e613b56a429c17
William Roberts <werobert@yahoo.com>
2025-04-14 13:51:08 -0400

    product-list comp. getbycategory

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'product-list',
@@ -10,11 +11,18 @@ import { ProductCardComponent } from '../product-card/product-card.component';
 })
 export class ProductListComponent implements OnInit {
   productService = inject(ProductService);
+  private router = inject(ActivatedRoute);
 
   ngOnInit(): void {
-    this.loadProducts();
+    if (this.router.snapshot.paramMap.has('id')) {
+      const categoryId: string = this.router.snapshot.paramMap.get('id')!;
+      this.searchByCategoryId(categoryId);
+    }
   }
 
+  searchByCategoryId(id: string) {
+    this.productService;
+  }
   loadProducts() {
     this.productService.getProducts();
   }
