815f8860bc6a70968dfa764f71a73068c02fed1d
William Roberts <werobert@yahoo.com>
2025-04-16 16:44:15 -0400

    okta-widget init

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,7 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { OKTA_AUTH } from '@okta/okta-angular';
 import OktaAuth from '@okta/okta-auth-js';
-import OctaSignIn, { OktaSignIn } from '@okta/okta-signin-widget';
+import {
+  OktaSignIn,
+  RenderError,
+  RenderResult,
+} from '@okta/okta-signin-widget';
 import appConfig from '../../configs/app-config';
 
 @Component({
@@ -10,10 +14,10 @@ import appConfig from '../../configs/app-config';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  private okta: OktaAuth = inject(OKTA_AUTH);
+export class LoginComponent implements OnInit {
+  private oktaAuth: OktaAuth = inject(OKTA_AUTH);
 
-  oktaSignIn = new OktaSignIn({
+  oktaSignIn: OktaSignIn = new OktaSignIn({
     logo: 'assets/images/musicnote.png',
     baseUrl: appConfig.oidc.issuer.split('/oauth2')[0],
     clientId: appConfig.oidc.clientId,
@@ -24,4 +28,17 @@ export class LoginComponent {
       scopes: appConfig.oidc.scopes,
     },
   });
+
+  ngOnInit(): void {
+    this.oktaSignIn.remove();
+    this.oktaSignIn.renderEl(
+      { el: '#okta-signin-widget' },
+      (res: RenderResult) => {
+        if (res.status === 'SUCCESS') this.oktaAuth.signInWithRedirect();
+      },
+      (error: RenderError) => {
+        throw error;
+      }
+    );
+  }
 }
