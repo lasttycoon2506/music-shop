d22f680684944114a46838e4272d4c4ec406718a
William Roberts <werobert@yahoo.com>
2025-04-16 15:16:21 -0400

    okta config set-up

---
+++ frontend/src/app/configs/app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oaobaw46qHcC3sVf5d7',
+    issuer: 'https://dev-52601989.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
