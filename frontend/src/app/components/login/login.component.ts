db99cc19803343b11b9db9821845114703f71263
William Roberts <werobert@yahoo.com>
2025-04-16 16:24:39 -0400

    oktaSignIn prop

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import OktaAuth from '@okta/okta-auth-js';
+import OctaSignIn, { OktaSignIn } from '@okta/okta-signin-widget';
+import appConfig from '../../configs/app-config';
 
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private okta: OktaAuth = inject(OKTA_AUTH);
 
+  oktaSignIn = new OktaSignIn({
+    logo: 'assets/images/musicnote.png',
+    baseUrl: appConfig.oidc.issuer.split('/oauth2')[0],
+    clientId: appConfig.oidc.clientId,
+    redirectUri: appConfig.oidc.redirectUri,
+    authParams: {
+      pkce: true,
+      issuer: appConfig.oidc.issuer,
+      scopes: appConfig.oidc.scopes,
+    },
+  });
 }
