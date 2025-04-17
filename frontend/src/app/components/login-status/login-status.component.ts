e2e2b8a338951cd8f1bc2bdff7ff9844984bd390
William Roberts <werobert@yahoo.com>
2025-04-17 10:48:31 -0400

    login-status comp.

---
+++ frontend/src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-status',
+  imports: [],
+  templateUrl: './login-status.component.html',
+  styleUrl: './login-status.component.css'
+})
+export class LoginStatusComponent {
+
+}
