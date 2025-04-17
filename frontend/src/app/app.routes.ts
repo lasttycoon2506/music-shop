af7e2b2d7b47df538313ced1736b119874e44d55
William Roberts <werobert@yahoo.com>
2025-04-17 11:45:10 -0400

    login routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,18 @@ import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { productDetailResolver } from './resolvers/product-detail-resolver.resolver';
+import { OktaCallbackComponent } from '@okta/okta-angular';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-  { path: '', component: ProductListComponent },
+  {
+    path: 'login/callback',
+    component: OktaCallbackComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
   {
     path: 'products/:id',
     resolve: { product: productDetailResolver },
@@ -18,4 +27,5 @@ export const routes: Routes = [
     path: 'products/search/:name',
     component: ProductListComponent,
   },
+  { path: '', component: ProductListComponent },
 ];
--- frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginStatusComponent } from '../login-status/login-status.component';
 
 @Component({
   selector: 'nav-bar',
-  imports: [],
+  imports: [LoginStatusComponent],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrl: './nav-bar.component.css',
 })
-export class NavBarComponent {
-
-}
+export class NavBarComponent {}
