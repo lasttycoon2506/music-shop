5b761274640f21665e7409dfdeab15ea0783b3e0
William Roberts <werobert@yahoo.com>
2025-04-17 12:00:32 -0400

    appconfig okta

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import appOktaConfig from './configs/app-config';
+import OktaAuth from '@okta/okta-auth-js';
+import { OktaAuthModule } from '@okta/okta-angular';
+
+const oktaConfig = appOktaConfig.oidc;
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,5 +20,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withFetch()),
+    importProvidersFrom(OktaAuthModule.forRoot({ oktaAuth })),
   ],
 };
