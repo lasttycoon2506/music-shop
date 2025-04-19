e8795c964484a024738bedbf061195bfe7f5f895
William Roberts <werobert@yahoo.com>
2025-04-19 13:08:37 -0400

    removed client-hydration

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,7 +5,6 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import appOktaConfig from './configs/app-config';
 import OktaAuth from '@okta/okta-auth-js';
@@ -18,7 +17,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    // provideClientHydration(),
     provideHttpClient(withFetch()),
     importProvidersFrom(OktaAuthModule.forRoot({ oktaAuth })),
   ],
