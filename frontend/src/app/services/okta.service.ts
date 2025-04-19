71a93612839949cc4120c310d183e4d0bf3032ec
William Roberts <werobert@yahoo.com>
2025-04-19 13:26:11 -0400

    oktaService

--- frontend/src/app/services/okta.service.ts
+++ frontend/src/app/services/okta.service.ts
@@ -1,6 +1,20 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import OktaAuth from '@okta/okta-auth-js';
 
 @Injectable({
   providedIn: 'root',
 })
-export class OktaService {}
+export class OktaService {
+  private oktaAuth: OktaAuth = inject(OKTA_AUTH);
+
+  createUser(user: any) {
+    try {
+      const transaction = this.oktaAuth.idx.register(user);
+      return transaction;
+    } catch (error: any) {
+      console.error(error);
+      throw error;
+    }
+  }
+}
