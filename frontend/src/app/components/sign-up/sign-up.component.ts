4d7b136168a9beefd3b648cd873b023d0625c898
William Roberts <werobert@yahoo.com>
2025-04-19 13:49:01 -0400

    signup ngmodel

--- frontend/src/app/components/sign-up/sign-up.component.ts
+++ frontend/src/app/components/sign-up/sign-up.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { OktaService } from '../../services/okta.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
+  private oktaService = inject(OktaService);
+  user: any;
 
+  onSubmit() {
+    console.log(this.user);
+    // this.oktaService.createUser(this.user);
+  }
 }
