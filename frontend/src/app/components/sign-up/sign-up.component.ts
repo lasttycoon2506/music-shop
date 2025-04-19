678143b864349655d1116ac359178532ea51c60b
William Roberts <werobert@yahoo.com>
2025-04-19 14:05:42 -0400

    removed user-model

--- frontend/src/app/components/sign-up/sign-up.component.ts
+++ frontend/src/app/components/sign-up/sign-up.component.ts
@@ -1,19 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { OktaService } from '../../services/okta.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
   private oktaService = inject(OktaService);
-  user: any;
+  private formBuilder: FormBuilder = inject(FormBuilder);
 
   onSubmit() {
-    console.log(this.user);
     // this.oktaService.createUser(this.user);
   }
 }
--- frontend/src/app/models/user.ts
+++
@@ -1,6 +0,0 @@
-export type User = {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-};
