5aa795df81f41d43774f96766dd1ff16c2e6bb39
William Roberts <werobert@yahoo.com>
2025-04-23 14:39:33 -0400

    checkout service

--- backend/src/main/java/com/example/demo/dto/PurchaseResponse.java
+++ backend/src/main/java/com/example/demo/dto/PurchaseResponse.java
@@ -4,5 +4,5 @@ import lombok.Data;
 
 @Data
 public class PurchaseResponse {
-    private final Long orderTrackingNumber;
+    private final String orderTrackingNumber;
 }
--- backend/src/main/java/com/example/demo/service/CheckoutService.java
+++ backend/src/main/java/com/example/demo/service/CheckoutService.java
@@ -1,8 +1,16 @@
 package com.example.demo.service;
 
+import java.util.Set;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.CustomerRepository;
+import com.example.demo.dto.Purchase;
+import com.example.demo.dto.PurchaseResponse;
+import com.example.demo.entity.Customer;
+import com.example.demo.entity.Order;
+import com.example.demo.entity.OrderItem;
 
 @Service
 public class CheckoutService {
@@ -10,5 +18,26 @@ public class CheckoutService {
 
     public CheckoutService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
+    }
+
+    public PurchaseResponse MakePurchase(Purchase purchase) {
+        Order order = purchase.getOrder();
+        String trackingNumber = generateOrderTrackingNumber();
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        Customer customer = purchase.getCustomer();
+
+        order.setTrackingNumber(trackingNumber);
+        order.setShippingAddress(purchase.getShippingAddress());
+        order.setBillingAddress(purchase.getBillingAddress());
+        orderItems.forEach(item -> order.addItem(item));
+        customer.addOrder(order);
+
+        customerRepository.save(customer);
+
+        return new PurchaseResponse(trackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+        return UUID.randomUUID().toString();
     }
 }
