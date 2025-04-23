51c6217249f6f54264b39c4a2b6f6b153a653aeb
William Roberts <werobert@yahoo.com>
2025-04-23 11:10:53 -0400

    addr-entity fields

--- backend/src/main/java/com/example/demo/entity/Address.java
+++ backend/src/main/java/com/example/demo/entity/Address.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -17,5 +19,21 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "zip")
+    private String zip;
+
+    // @OneToOne
+    // @PrimaryKeyJoinColumn
+    // private Order order;
 
 }
