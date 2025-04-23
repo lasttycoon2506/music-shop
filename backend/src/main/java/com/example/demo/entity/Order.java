67002c590fca656ca660f2996f702b230d2ad46d
William Roberts <werobert@yahoo.com>
2025-04-23 11:44:31 -0400

    customer/orderItem entity

--- backend/src/main/java/com/example/demo/entity/Order.java
+++ backend/src/main/java/com/example/demo/entity/Order.java
@@ -2,15 +2,20 @@ package com.example.demo.entity;
 
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.util.Set;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -42,5 +47,12 @@ public class Order {
     @Column(name = "lastUpdated")
     @UpdateTimestamp
     private Date lastUpdated;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "order")
+    private Set<OrderItem> orderItems;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
 
 }
