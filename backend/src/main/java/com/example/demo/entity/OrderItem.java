664af7b8f6d4dead43f2044dbdd5c3e283a6e364
William Roberts <werobert@yahoo.com>
2025-04-23 12:47:46 -0400

    order-item entity

--- backend/src/main/java/com/example/demo/entity/OrderItem.java
+++ backend/src/main/java/com/example/demo/entity/OrderItem.java
@@ -1,5 +1,39 @@
 package com.example.demo.entity;
 
-public class OrderItem {
+import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Table(name = "order_items")
+@Entity
+@Data
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "imageUrl")
+    private String imageUrl;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "productId")
+    private Long productId;
+
+    @ManyToOne()
+    @JoinColumn(name = "order_id")
+    private Order order;
 }
