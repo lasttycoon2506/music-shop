96251ffcfc48a91bf98fe67b58bebe7e2197728e
William Roberts <werobert@yahoo.com>
2025-04-24 12:11:19 -0400

    sql seed tables upd.

--- backend/order-customer-address.sql
+++ backend/order-customer-address.sql
@@ -34,7 +34,7 @@ CREATE TABLE `customers` (
 --
 CREATE TABLE `orders` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `order_tracking_number` varchar(255) DEFAULT NULL,
+  `tracking_number` varchar(255) DEFAULT NULL,
   `total_price` decimal(19,2) DEFAULT NULL,
   `total_quantity` int DEFAULT NULL,
   `billing_address_id` bigint DEFAULT NULL,
@@ -47,23 +47,23 @@ CREATE TABLE `orders` (
   UNIQUE KEY `UK_billing_address_id` (`billing_address_id`),
   UNIQUE KEY `UK_shipping_address_id` (`shipping_address_id`),
   KEY `K_customer_id` (`customer_id`),
-  CONSTRAINT `FK_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
-  CONSTRAINT `FK_billing_address_id` FOREIGN KEY (`billing_address_id`) REFERENCES `address` (`id`),
-  CONSTRAINT `FK_shipping_address_id` FOREIGN KEY (`shipping_address_id`) REFERENCES `address` (`id`)
+  CONSTRAINT `FK_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
+  CONSTRAINT `FK_billing_address_id` FOREIGN KEY (`billing_address_id`) REFERENCES `addresses` (`id`),
+  CONSTRAINT `FK_shipping_address_id` FOREIGN KEY (`shipping_address_id`) REFERENCES `addresses` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Table structure for table `order_items`
+-- order_items table
 --
-CREATE TABLE `order_item` (
+CREATE TABLE `order_items` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `image_url` varchar(255) DEFAULT NULL,
   `quantity` int DEFAULT NULL,
-  `unit_price` decimal(19,2) DEFAULT NULL,
+  `price` decimal(19,2) DEFAULT NULL,
   `order_id` bigint DEFAULT NULL,
   `product_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `K_order_id` (`order_id`),
   CONSTRAINT `FK_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
-  CONSTRAINT `FK_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+  CONSTRAINT `FK_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
