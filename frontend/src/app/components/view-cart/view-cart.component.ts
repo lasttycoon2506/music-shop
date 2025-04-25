d6587894b2c5d7bb7a64a71878047cf4e70af234
William Roberts <werobert@yahoo.com>
2025-04-25 14:49:42 -0400

    view-cart component

---
+++ frontend/src/app/components/view-cart/view-cart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-cart',
+  imports: [],
+  templateUrl: './view-cart.component.html',
+  styleUrl: './view-cart.component.css'
+})
+export class ViewCartComponent {
+
+}
