d6705e07e98df73df903d77d94aac7d09e0399c5
William Roberts <werobert@yahoo.com>
2025-04-25 16:33:38 -0400

    cart-item-card style

--- frontend/src/app/components/cart-item-card/cart-item-card.component.ts
+++ frontend/src/app/components/cart-item-card/cart-item-card.component.ts
@@ -1,12 +1,28 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, InputSignal, OnInit } from '@angular/core';
 import { OrderItem } from '../../models/orderItem';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'cart-item-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart-item-card.component.html',
   styleUrl: './cart-item-card.component.css',
 })
-export class CartItemCardComponent {
-  item = input.required<OrderItem>();
+export class CartItemCardComponent implements OnInit {
+  calculateItemTotal() {
+    throw new Error('Method not implemented.');
+  }
+  private productService = inject(ProductService);
+  item: InputSignal<OrderItem> = input.required<OrderItem>();
+  product: Product | null = null;
+
+  ngOnInit(): void {
+    this.productService
+      .getProductDetail(this.item().productId.toString())
+      .subscribe({
+        next: (product: Product) => (this.product = product),
+      });
+  }
 }
