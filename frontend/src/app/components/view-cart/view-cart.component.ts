9294ea7fa2de926bb57e65bffeca650a667f6783
William Roberts <werobert@yahoo.com>
2025-04-25 17:42:43 -0400

    total-cart

--- frontend/src/app/components/view-cart/view-cart.component.ts
+++ frontend/src/app/components/view-cart/view-cart.component.ts
@@ -1,13 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { CheckOutService } from '../../services/check-out.service';
 import { CartItemCardComponent } from '../cart-item-card/cart-item-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-cart',
-  imports: [CartItemCardComponent],
+  imports: [CartItemCardComponent, CommonModule],
   templateUrl: './view-cart.component.html',
   styleUrl: './view-cart.component.css',
 })
 export class ViewCartComponent {
   checkoutService = inject(CheckOutService);
+
+  CalculateCartTotal(): number | undefined {
+    const total = this.checkoutService
+      .order()
+      ?.orderItems?.reduce(
+        (total, item) => total + item.price * item.quantity,
+        0
+      );
+
+    return total;
+  }
 }
