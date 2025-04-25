7f30b243183fc8cd9428c1c1ebc1903ffbb34709
William Roberts <werobert@yahoo.com>
2025-04-25 14:52:36 -0400

    cart-item-card component

---
+++ frontend/src/app/components/cart-item-card/cart-item-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-item-card',
+  imports: [],
+  templateUrl: './cart-item-card.component.html',
+  styleUrl: './cart-item-card.component.css'
+})
+export class CartItemCardComponent {
+
+}
