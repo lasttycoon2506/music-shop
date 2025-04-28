6b82c08b93d43168be6481e54ba4cf18fc92be9d
William Roberts <werobert@yahoo.com>
2025-04-28 11:01:06 -0400

    checkoutcart price

--- frontend/src/app/components/check-out/check-out.component.ts
+++ frontend/src/app/components/check-out/check-out.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CheckOutService } from '../../services/check-out.service';
 
 @Component({
   selector: 'app-check-out',
   imports: [],
   templateUrl: './check-out.component.html',
-  styleUrl: './check-out.component.css'
+  styleUrl: './check-out.component.css',
 })
 export class CheckOutComponent {
-
+  checkoutService = inject(CheckOutService);
 }
