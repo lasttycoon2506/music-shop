bacd8aee4828544cb5dcf9fe536fbea92bc2413a
William Roberts <werobert@yahoo.com>
2025-04-28 11:30:10 -0400

    products-array init

--- frontend/src/app/components/check-out/check-out.component.ts
+++ frontend/src/app/components/check-out/check-out.component.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CheckOutService } from '../../services/check-out.service';
+import { Product } from '../../models/product';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-check-out',
@@ -7,6 +9,18 @@ import { CheckOutService } from '../../services/check-out.service';
   templateUrl: './check-out.component.html',
   styleUrl: './check-out.component.css',
 })
-export class CheckOutComponent {
+export class CheckOutComponent implements OnInit {
   checkoutService = inject(CheckOutService);
+  productService = inject(ProductService);
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.checkoutService
+      .order()
+      ?.orderItems?.forEach((item) =>
+        this.productService
+          .getProductDetail(item.productId.toString())
+          .subscribe({ next: (product) => this.products.push(product) })
+      );
+  }
 }
