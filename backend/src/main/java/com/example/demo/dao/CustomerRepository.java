7e1bfadd5d97125fb609d3387afe83a1097e7569
William Roberts <werobert@yahoo.com>
2025-05-02 12:51:37 -0400

    customer repo edit cust

--- backend/src/main/java/com/example/demo/dao/CustomerRepository.java
+++ backend/src/main/java/com/example/demo/dao/CustomerRepository.java
@@ -1,9 +1,16 @@
 package com.example.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import com.example.demo.entity.Address;
 import com.example.demo.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    @Modifying
+    @Query("update Customer c set c.firstName = ?1, c.lastName = ?2, c.email = ?3, c.billingAddress = ?4, c.shippingAddress = ?5 where c.email = ?3")
+    int setUserInfoById(String firstName, String lastName, String email, Address billingAddress,
+            Address shippingAddress);
 
 }
--- backend/src/main/java/com/example/demo/service/CustomerService.java
+++ backend/src/main/java/com/example/demo/service/CustomerService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.dao.CustomerRepository;
 import com.example.demo.dto.CustomerDto;
+import com.example.demo.entity.Customer;
 
 public class CustomerService {
     private CustomerRepository customerRepository;
@@ -10,7 +11,8 @@ public class CustomerService {
         this.customerRepository = customerRepository;
     }
 
-    public void editCustomer(CustomerDto customer) {
+    public void editCustomer(CustomerDto customerDto) {
 
     }
+
 }
