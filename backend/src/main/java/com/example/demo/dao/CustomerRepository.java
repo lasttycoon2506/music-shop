9d58763b7ed1907c3e848f838864e87035bcb41e
William Roberts <werobert@yahoo.com>
2025-05-02 17:15:43 -0400

    findByEmail on email entity

--- backend/src/main/java/com/example/demo/dao/CustomerRepository.java
+++ backend/src/main/java/com/example/demo/dao/CustomerRepository.java
@@ -3,9 +3,15 @@ package com.example.demo.dao;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.demo.entity.Customer;
 
+@CrossOrigin("http://localhost:4200")
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findByEmail(String email);
+
+    Customer findByEmailContaining(@Param("email") String name);
+
 }
