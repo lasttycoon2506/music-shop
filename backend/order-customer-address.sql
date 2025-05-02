db7c508ddfbb4944639fabe3605af689fa61fd46
William Roberts <werobert@yahoo.com>
2025-05-02 11:45:17 -0400

    removed 1-1 order entity

--- backend/order-customer-address.sql
+++ backend/order-customer-address.sql
@@ -49,7 +49,7 @@ CREATE TABLE `orders` (
   `last_updated` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `K_customer_id` (`customer_id`),
-  CONSTRAINT `FK_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
+  CONSTRAINT `FK_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
