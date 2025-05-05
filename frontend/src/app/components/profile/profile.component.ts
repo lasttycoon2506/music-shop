315b18e959530f9394267242ac25e935e773a861
William Roberts <werobert@yahoo.com>
2025-05-05 14:29:09 -0400

    profile-form

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -2,10 +2,16 @@ import { Component, inject } from '@angular/core';
 import { STATES_ABBREVIATIONS } from '../../constants/states.constants';
 import { OktaService } from '../../services/okta.service';
 import { CustomerService } from '../../services/customer.service';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
@@ -14,6 +20,14 @@ export class ProfileComponent {
   customerService = inject(CustomerService);
   billingShippingSame: boolean = false;
   STATES_ABBREVIATIONS = STATES_ABBREVIATIONS;
+  profileForm: FormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    street: new FormControl(''),
+    city: new FormControl(''),
+    state: new FormControl(''),
+    zip: new FormControl('', Validators.minLength(5)),
+  });
 
   billingShippingAddrSame(event: Event) {
     const isChecked: boolean = (event.target as HTMLInputElement).checked;
@@ -22,5 +36,9 @@ export class ProfileComponent {
     } else {
       this.billingShippingSame = false;
     }
+  }
+
+  editCustomer() {
+    console.log('working');
   }
 }
