73a505701b7e46e5f05c7bde71876c1d8ce2e58d
William Roberts <werobert@yahoo.com>
2025-05-05 11:43:12 -0400

    gets cur-customer

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { STATES_ABBREVIATIONS } from '../../constants/states.constants';
 import { OktaService } from '../../services/okta.service';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,5 +11,6 @@ import { OktaService } from '../../services/okta.service';
 })
 export class ProfileComponent {
   oktaService = inject(OktaService);
+  customerService = inject(CustomerService);
   STATES_ABBREVIATIONS = STATES_ABBREVIATIONS;
 }
