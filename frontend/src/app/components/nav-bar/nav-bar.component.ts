1a1eb006cac6997e5692ac8cbb0fe9962b6bf457
William Roberts <werobert@yahoo.com>
2025-05-06 16:38:13 -0400

    home-navbar no-refresh

--- frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LoginStatusComponent } from '../login-status/login-status.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'nav-bar',
-  imports: [LoginStatusComponent],
+  imports: [LoginStatusComponent, RouterLink],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 })
