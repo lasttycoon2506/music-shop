abff3c525315859fc9501558071e6675f8b9ef7a
William Roberts <werobert@yahoo.com>
2025-05-06 16:59:27 -0400

    checkout-form fields

--- frontend/src/app/components/check-out/check-out.component.ts
+++ frontend/src/app/components/check-out/check-out.component.ts
@@ -5,6 +5,8 @@ import { ProductService } from '../../services/product.service';
 import { CommonModule } from '@angular/common';
 import { ParseProductId } from '../../helpers/parseProductId';
 import { STATES_ABBREVIATIONS } from '../../constants/states.constants';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-check-out',
@@ -14,9 +16,61 @@ import { STATES_ABBREVIATIONS } from '../../constants/states.constants';
 })
 export class CheckOutComponent implements OnInit {
   private productService = inject(ProductService);
+  private customerService = inject(CustomerService);
   checkoutService = inject(CheckOutService);
+  billingShippingSame: boolean = false;
   products: Product[] = [];
   STATES_ABBREVIATIONS = STATES_ABBREVIATIONS;
+  checkoutForm: FormGroup = new FormGroup({
+    billingFirstName: new FormControl(
+      this.customerService.currentCustomer()?.billingAddress?.firstName ?? '',
+      Validators.required
+    ),
+    billingLastName: new FormControl(
+      this.customerService.currentCustomer()?.billingAddress?.lastName ?? '',
+      Validators.required
+    ),
+    billingStreet: new FormControl(
+      this.customerService.currentCustomer()?.billingAddress?.street ?? '',
+      Validators.required
+    ),
+    billingCity: new FormControl(
+      this.customerService.currentCustomer()?.billingAddress?.city ?? '',
+      Validators.required
+    ),
+    billingState: new FormControl(
+      this.customerService.currentCustomer()?.billingAddress?.state ?? '',
+      Validators.required
+    ),
+    billingZip: new FormControl(
+      this.customerService.currentCustomer()?.billingAddress?.zip ?? '',
+      [Validators.minLength(5), Validators.maxLength(5), Validators.required]
+    ),
+    shippingFirstName: new FormControl(
+      this.customerService.currentCustomer()?.shippingAddress?.firstName ?? '',
+      Validators.required
+    ),
+    shippingLastName: new FormControl(
+      this.customerService.currentCustomer()?.shippingAddress?.lastName ?? '',
+      Validators.required
+    ),
+    shippingStreet: new FormControl(
+      this.customerService.currentCustomer()?.shippingAddress?.street ?? '',
+      Validators.required
+    ),
+    shippingCity: new FormControl(
+      this.customerService.currentCustomer()?.shippingAddress?.city ?? '',
+      Validators.required
+    ),
+    shippingState: new FormControl(
+      this.customerService.currentCustomer()?.shippingAddress?.state ?? '',
+      Validators.required
+    ),
+    shippingZip: new FormControl(
+      this.customerService.currentCustomer()?.shippingAddress?.zip ?? '',
+      [Validators.minLength(5), Validators.maxLength(5), Validators.required]
+    ),
+  });
 
   ngOnInit(): void {
     this.checkoutService.order()?.orderItems?.forEach((item) =>
