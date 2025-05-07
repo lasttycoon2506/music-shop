cdb8301f45755441b6c55ae3c628d75c99561523
William Roberts <werobert@yahoo.com>
2025-05-07 16:51:15 -0400

    order-card

--- frontend/src/app/components/orders/orders.component.ts
+++ frontend/src/app/components/orders/orders.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { OrderCardComponent } from '../order-card/order-card.component';
 
 @Component({
   selector: 'app-orders',
-  imports: [],
+  imports: [OrderCardComponent],
   templateUrl: './orders.component.html',
-  styleUrl: './orders.component.css'
+  styleUrl: './orders.component.css',
 })
-export class OrdersComponent {
-
-}
+export class OrdersComponent {}
