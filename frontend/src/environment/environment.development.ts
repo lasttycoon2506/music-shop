ca41b0753b5626e9c6e59be9e4f77fd8815c849f
William Roberts <werobert@yahoo.com>
2025-05-09 16:26:52 -0400

    stripe publishable-key

--- frontend/src/environment/environment.development.ts
+++ frontend/src/environment/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api/',
+  StripePublishableKey:
+    'pk_test_51RMZ2AC54lK8mKCTp4yZaB93M9dZEh81zHCKC8pfuaJI19ETiNGvzZClv32B3g9zEdUiN53Hk0pcVCYqg2V4CQl6003jtLTWTL',
 };
