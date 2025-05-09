1c918badc036f9564b38da3272a772ca5a588d3b
William Roberts <werobert@yahoo.com>
2025-05-09 16:50:14 -0400

    create-payment-intent checkoutserv

--- frontend/src/app/services/check-out.service.ts
+++ frontend/src/app/services/check-out.service.ts
@@ -1,13 +1,23 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { Order } from '../models/order';
+import { PaymentDto } from '../models/paymentDto';
+import { HttpClient } from '@angular/common/http';
+import { PaymentIntentResult } from '@stripe/stripe-js';
+import { environment } from '../../environment/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CheckOutService {
+  private httpClient = inject(HttpClient);
   order: WritableSignal<Order | null> = signal<Order | null>({
     orderItems: [],
   });
 
-  createPaymentIntent() {}
+  createPaymentIntent(payment: PaymentDto) {
+    return this.httpClient.post<PaymentIntentResult>(
+      environment.apiUrl + 'checkout/payment-intent',
+      payment
+    );
+  }
 }
