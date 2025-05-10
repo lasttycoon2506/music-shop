0c527ad564ff37ed2fce73b56498bcd0273d6ded
William Roberts <werobert@yahoo.com>
2025-05-10 13:49:07 -0400

    checkout-service types

--- frontend/src/app/services/check-out.service.ts
+++ frontend/src/app/services/check-out.service.ts
@@ -4,17 +4,18 @@ import { PaymentDto } from '../models/paymentDto';
 import { HttpClient } from '@angular/common/http';
 import { PaymentIntentResult } from '@stripe/stripe-js';
 import { environment } from '../../environment/environment.development';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CheckOutService {
-  private httpClient = inject(HttpClient);
+  private httpClient: HttpClient = inject(HttpClient);
   order: WritableSignal<Order | null> = signal<Order | null>({
     orderItems: [],
   });
 
-  createPaymentIntent(payment: PaymentDto) {
+  createPaymentIntent(payment: PaymentDto): Observable<PaymentIntentResult> {
     return this.httpClient.post<PaymentIntentResult>(
       environment.apiUrl + 'checkout/payment-intent',
       payment
