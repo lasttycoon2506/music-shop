ec445efbbd6e68e6147033a2967095e2a25ebcda
William Roberts <werobert@yahoo.com>
2025-05-13 12:15:00 -0400

    removed addrr from Order-type

--- frontend/src/app/models/order.ts
+++ frontend/src/app/models/order.ts
@@ -1,19 +1,10 @@
 import { OrderItem } from './orderItem';
 
 export type Order = {
-  shippingAddress?: Address;
-  billingAddress?: Address;
   order?: {
     totalQuantity: number;
     totalPrice?: number;
     status?: string;
   };
   orderItems?: OrderItem[];
-};
-
-type Address = {
-  street: string;
-  city: string;
-  state: string;
-  zip: string;
 };
