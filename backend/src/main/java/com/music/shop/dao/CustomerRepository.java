717f4a44fed935b5797c4b4c705ecf753c226e9a
William Roberts <werobert@yahoo.com>
2025-05-14 13:00:22 -0400

    sort orders-date-created

--- backend/src/main/java/com/music/shop/dao/CustomerRepository.java
+++ backend/src/main/java/com/music/shop/dao/CustomerRepository.java
@@ -3,6 +3,7 @@ package com.music.shop.dao;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -12,4 +13,6 @@ import com.music.shop.entity.Customer;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findByEmail(@Param("email") String email);
 
+    @Query("SELECT c FROM Customer c LEFT JOIN FETCH c.orders o WHERE c.email = :email ORDER BY o.dateCreated ASC")
+    Optional<Customer> findByEmailSortedByDateCreated(@Param("email") String email);
 }
